c68bb05cc79fe3ebc45543cdb322cc6128b607af
Efraim Nabil <efraimefefraim@gmail.com>
2023-07-02 23:14:29 +0300

    update settings

--- src/component/Settings.js
+++ src/component/Settings.js
@@ -7,11 +7,10 @@ const Settings = () => {
     rate: '',
     problemsCount: ''
   });
-  const [error, setError] = useState("");
-  const [isButtonActive, setIsButtonActive] = useState(false);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user");
+    const storedUser = localStorage.getItem('user');
     if (storedUser) {
       const parsedUser = JSON.parse(storedUser);
       setUser(parsedUser);
@@ -23,29 +22,26 @@ const Settings = () => {
       const response = await fetch(`https://codeforces.com/api/user.info?handles=${user.handle}`);
       if (response.ok) {
         const data = await response.json();
-        if (data.status === "OK") {
-          setError("");
-          setIsButtonActive(true);
+        if (data.status === 'OK') {
+          setError('');
           return true;
         } else {
           setError(`Invalid handle: ${user.handle}`);
-          setIsButtonActive(false);
           return false;
         }
       } else {
-        throw new Error(`Request failed with status ${response.status}`);
+        setError(`Invalid handle: ${user.handle}`);
+        return false;
       }
     } catch (error) {
-      setError(`Error: ${error.message}`);
-      setIsButtonActive(false);
+      setError('An error occurred while checking the handle validity.');
       return false;
     }
   }, [user.handle]);
-  
 
   const checkRateValidity = useCallback(() => {
     if (user.rate < 800 || user.rate > 3500) {
-      setError("Please enter a valid rate between 800 and 3500.");
+      setError('Please enter a valid rate between 800 and 3500.');
       return false;
     }
     return true;
@@ -53,84 +49,72 @@ const Settings = () => {
 
   const checkProblemsCountValidity = useCallback(() => {
     if (user.problemsCount < 1 || user.problemsCount > 10) {
-      setError("Please enter a valid number of problems between 1 and 10.");
+      setError('Please enter a valid number of problems between 1 and 10.');
       return false;
     }
     return true;
   }, [user.problemsCount]);
 
   const handleUpdate = async () => {
-    if (checkHandleValidity() && checkRateValidity() && checkProblemsCountValidity()) {
-      const newUser = {
-        ...user,
-        loggedIn: true,
-      };
-      localStorage.setItem("user", JSON.stringify(newUser));
-      window.location.reload();
+    if (user.handle.trim() === '') {
+      setError('Please enter a Codeforces handle.');
+      return;
     }
+
+    if (!checkRateValidity() || !checkProblemsCountValidity()) {
+      return;
+    }
+
+    const isValid = await checkHandleValidity();
+    if (!isValid) {
+      return;
+    }
+
+    const newUser = { ...user, loggedIn: true };
+    localStorage.setItem('user', JSON.stringify(newUser));
+    setUser(newUser);
+    window.location.reload();
   };
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setUser({
-      ...user,
+    setUser((prevUser) => ({
+      ...prevUser,
       [name]: value,
-    });
+    }));
   };
 
-  useEffect(() => {
-    if (checkHandleValidity() && checkRateValidity() && checkProblemsCountValidity()) {
-      setIsButtonActive(true);
-    } else {
-      setIsButtonActive(false);
-    }
-  }, [user, checkHandleValidity, checkProblemsCountValidity, checkRateValidity]);
+  const renderForm = () => (
+    <form className="settings__form">
+      <label>
+        Codeforces Handle:
+        <input type="text" name="handle" value={user.handle} onChange={handleInputChange} />
+      </label>
+      <label>
+        Rate of Problems:
+        <input type="text" name="rate" value={user.rate} onChange={handleInputChange} />
+      </label>
+      <label>
+        Number of Problems:
+        <input
+          type="text"
+          name="problemsCount"
+          value={user.problemsCount}
+          onChange={handleInputChange}
+        />
+      </label>
+    </form>
+  );
 
-  const renderForm = () => {
-    return (
-      <form className="settings__form">
-        <label>
-          Codeforces Handle:
-          <input
-            type="text"
-            name="handle"
-            value={user.handle}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Rate of Problems:
-          <input
-            type="text"
-            name="rate"
-            value={user.rate}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Number of Problems:
-          <input
-            type="text"
-            name="problemsCount"
-            value={user.problemsCount}
-            onChange={handleInputChange}
-          />
-        </label>
-      </form>
-    );
-  };
-
-  const renderSettings = () => {
-    return (
-      <div className="settings">
-        {renderForm()}
-        {error && <p className="error">{error}</p>}
-        <button className="update-btn" onClick={handleUpdate} disabled={!isButtonActive}>
-          Update
-        </button>
-      </div>
-    );
-  };
+  const renderSettings = () => (
+    <div className="settings">
+      {renderForm()}
+      <button className="update-btn" onClick={handleUpdate}>
+        Update
+      </button>
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
 
   return (
     <div className="settings">
