04f28956d36fa654904ff74785f963c45d9f9239
Efraim Nabil <efraimefefraim@gmail.com>
2023-07-10 05:05:00 +0300

    update loader

--- src/component/Loader.js
+++ src/component/Loader.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./../styles/Loader.css";
 
 const Loader = () => {
     return (
-        <div className="loader">
-            <div className="loader__spinner"></div>
+        <div className="loader-container">
+            <div className="loader"></div>
         </div>
     );
 };
