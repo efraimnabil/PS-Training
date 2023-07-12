1b2bc3997b40f93b6b0c3f541b34579aa4b9adc7
Efraim Nabil <efraimefefraim@gmail.com>
2023-07-12 15:43:13 +0300

    comments

--- src/component/Login.js
+++ src/component/Login.js
@@ -2,21 +2,21 @@ import React, { useState, useCallback } from "react";
 import "./../styles/Login.css";
 import circle from "./../images/circle.webp";
 import cupe from "./../images/cupe.webp";
-const Login = ({ handleLogin }) => {  // This component will render the login form
 
-  const [handle, setHandle] = useState(""); // This state will contain the handle
-  const [rateofProblems, setRate] = useState(""); // This state will contain the rate of the problems
-  const [problemsCount, setProblemsCount] = useState(""); // This state will contain the number of problems
+const Login = ({ handleLogin }) => {
+  const [handle, setHandle] = useState(""); // State for the handle input
+  const [rateofProblems, setRate] = useState(""); // State for the rate of problems input
+  const [problemsCount, setProblemsCount] = useState(""); // State for the number of problems input
 
   const checkHandleValidity = useCallback(async () => {
     try {
-      const response = await fetch(`https://codeforces.com/api/user.info?handles=${handle}`); // Fetch the user details from the Codeforces API
+      const response = await fetch(`https://codeforces.com/api/user.info?handles=${handle}`);
       if (response.ok) {
         const data = await response.json();
         if (data.status === 'OK') {
           return [];
         } else {
-          
+          // Handle is not valid, return an error message
         }
       } else {
         return ['An error occurred while checking the handle validity.'];
@@ -40,19 +40,22 @@ const Login = ({ handleLogin }) => {  // This component will render the login fo
     return false;
   }, [problemsCount]);
 
-  const handleSubmit = async (e) => {  // This function will be called when the user submits the form
-    e.preventDefault();  
-    let handleNotValid = false, rateNotValid = false, problemsCountNotValid = false; // These variables will be used to check the validity of the input
-    if (handle.trim() === "") { 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let handleNotValid = false,
+      rateNotValid = false,
+      problemsCountNotValid = false;
+
+    if (handle.trim() === "") {
       handleNotValid = true;
     }
 
-    if (rateofProblems.trim() === "" || checkRateValidity()) { 
-      rateNotValid = true; 
+    if (rateofProblems.trim() === "" || checkRateValidity()) {
+      rateNotValid = true;
     }
 
-    if (problemsCount.trim() === "" || checkProblemsCountValidity()) { 
-      problemsCountNotValid = true; 
+    if (problemsCount.trim() === "" || checkProblemsCountValidity()) {
+      problemsCountNotValid = true;
     }
 
     const handleValidityErrors = await checkHandleValidity();
@@ -61,96 +64,97 @@ const Login = ({ handleLogin }) => {  // This component will render the login fo
     }
 
     let handleDocLabel = document.querySelector('.handle__label'),
-        rateDocLabel = document.querySelector('.rate__label'),
-        problemsDocLabel = document.querySelector('.problems__label'),
-        handleDocInput = document.querySelector('.handle__input'),
-        rateDocInput = document.querySelector('.rate__input'),
-        problemsDocInput = document.querySelector('.problems__input');
+      rateDocLabel = document.querySelector('.rate__label'),
+      problemsDocLabel = document.querySelector('.problems__label'),
+      handleDocInput = document.querySelector('.handle__input'),
+      rateDocInput = document.querySelector('.rate__input'),
+      problemsDocInput = document.querySelector('.problems__input');
 
     if (handleNotValid) {
       handleDocLabel.style.color = 'red';
       handleDocLabel.innerHTML = 'Invalid Handle';
       handleDocInput.style.border = '1px solid red';
-    }
-    else{
+    } else {
       handleDocLabel.style.color = 'green';
       handleDocLabel.innerHTML = 'Codeforces Handle';
       handleDocInput.style.border = '1px solid green';
     }
+
     if (rateNotValid) {
       rateDocLabel.style.color = 'red';
       rateDocLabel.innerHTML = 'Invalid Rating';
       rateDocInput.style.border = '1px solid red';
-    }
-    else{
+    } else {
       rateDocLabel.style.color = 'green';
       rateDocLabel.innerHTML = 'Rating';
       rateDocInput.style.border = '1px solid green';
     }
+
     if (problemsCountNotValid) {
       problemsDocLabel.style.color = 'red';
       problemsDocLabel.innerHTML = 'Invalid Problems Count';
       problemsDocInput.style.border = '1px solid red';
-    }
-    else{
+    } else {
       problemsDocLabel.style.color = 'green';
       problemsDocLabel.innerHTML = 'Problems Count';
       problemsDocInput.style.border = '1px solid green';
     }
+
     if (handleNotValid || rateNotValid || problemsCountNotValid) {
       return;
     }
 
-    const user = {  
+    const user = {
       handle: handle,
       rate: rateofProblems,
       problemsCount: problemsCount,
       loggedIn: true,
     };
-    localStorage.setItem("user", JSON.stringify(user)); // Store the user in localStorage
-    handleLogin(); // Call the handleLogin function passed as a prop from App.js
+
+    localStorage.setItem("user", JSON.stringify(user));
+    handleLogin();
   };
-  
+
   return (
     <div className="container">
       <div className="login">
-      <div className="shape shape-1">
-        <img src={circle} alt="circle" />
-      </div>
-      <div className="shape shape-2">
-        <img src={cupe} alt="cupe" />
-      </div>
-      <form onSubmit={handleSubmit} className="login__form">
-      <h1 className="login__title">Login</h1>
+        <div className="shape shape-1">
+          <img src={circle} alt="circle" />
+        </div>
+        <div className="shape shape-2">
+          <img src={cupe} alt="cupe" />
+        </div>
+        <form onSubmit={handleSubmit} className="login__form">
+          <h1 className="login__title">Login</h1>
           <label className="login__label handle__label">Enter your Handle</label>
           <input
             type="text"
             name="handle"
             placeholder="Handle"
-            className = "login__input handle__input"
+            className="login__input handle__input"
             value={handle}
             onChange={(e) => setHandle(e.target.value)}
-            />
+          />
           <label className="login__label rate__label">Enter your Rating</label>
           <input
             type="text"
             name="rateOfProblems"
             placeholder="800-3500"
-            className = "login__input rate__input"
+            className="login__input rate__input"
             value={rateofProblems}
             onChange={(e) => setRate(e.target.value)}
-            />
+          />
           <label className="login__label problems__label">Enter the number of problems</label>
-          <input 
+          <input
             type="text"
             name="problemsCount"
             placeholder="1-10"
-            className = "login__input problems__input"
+            className="login__input problems__input"
             onChange={(e) => setProblemsCount(e.target.value)}
-            />
-        <button type="submit" className="login__btn">Login</button>
-      </form>
-    </div>
+          />
+          <button type="submit" className="login__btn">Login</button>
+        </form>
+      </div>
     </div>
   );
 };
